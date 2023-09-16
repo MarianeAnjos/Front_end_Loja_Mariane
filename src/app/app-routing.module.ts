9b89976c1016d513ff454916e43266a4982c597f
Mariane <mariane.ferreiraanjos@outlook.com>
2023-09-15 23:46:55 -0300

    iniciando o front-end angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProdutoComponent } from './views/produto/produto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProdutoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
